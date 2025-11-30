5ccba59e252195186e709ab8eed8b40a955d75a1
Ajith7736 <ajith.aju39502@gmail.com>
2025-11-30 23:40:26 +0530

    .

--- src/features/home/layout/Content.tsx
+++ src/features/home/layout/Content.tsx
@@ -44,13 +44,12 @@ function Content({ session }: { session: Session | null }) {
 
                 <motion.div initial={{ opacity: 0, translateY: -10 }} whileInView={{ opacity: 1, translateY: 0 }} viewport={{once : true}} className='relative z-10 flex flex-col gap-6 justify-center items-center p-10 lg:px-50 xl:px-80 w-full h-full'>
                     <div className="font-extrabold xss:text-4xl  md:text-7xl text-center">
-                        Analyze your code
-                        at the speed of AI
+                        Analyze your whole <span className="text-indigo-600">github</span> codebase at the speed of AI
                     </div>
                     <div className='text-center font-family-sans text-light-activeborder font-semibold xss:text-xs lg:text-sm'>Detect architecture flaws, security risks, and bottlenecks instantly. Get <span className=' text-indigo-600 italic text-base font-family-bebas'>AI-powered</span> refactoring suggestions directly in your repo.</div>
-                    <div className='flex gap-5 mt-5'>
+                    <div className='flex gap-5 mt-5 items-center'>
                         <Link href={session ? "/Dashboard" : "/Signup"}><button className={cn('bg-indigo-600 rounded-[3px] cursor-pointer hover:bg-indigo-700 transition duration-300 text-light-white p-3  xss:text-base xss:text-[11px] md:text-base font-extrabold text-white shadow-md shadow-indigo-500 dark:shadow-none')}>Start Your Analysis</button></Link>
-                        <Link href={"https://github.com/Ajith7736/codescope"} target="_blank"><button className='p-2 rounded-md cursor-pointer hover:bg-light-surface-hover hover:dark:bg-dark-surface-hover/50 transition duration-300 dark:backdrop-blur-2xl xss:text-sm md:text-base'>Github</button></Link>
+                        <Link href={"https://github.com/Ajith7736/codescope"} target="_blank"><button className='p-3 rounded-md cursor-pointer hover:bg-light-surface-hover hover:dark:bg-dark-surface-hover/50 transition duration-300 dark:backdrop-blur-2xl xss:text-sm md:text-base'>Github</button></Link>
                     </div>
                 </motion.div>
             </div>
