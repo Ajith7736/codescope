354f80dec53452eec6fec71b582202d850c5f9ea
Ajith7736 <ajith.aju39502@gmail.com>
2026-01-05 12:10:39 +0530

    seo

--- src/features/home/layout/Navbar.tsx
+++ src/features/home/layout/Navbar.tsx
@@ -16,7 +16,7 @@ function Navbar() {
                     <span className="">CodeScope AI</span>
                 </Link>
                 <div className='flex gap-5 items-center text-lg'>
-                    <Link href={"/Signup"} className='bg-light-surface px-4 py-1 cursor-pointer rounded-md hover:bg-light-surface-hover transition duration-300 dark:bg-dark-surface  dark:text-dark-text-primary text-light-text-primary hover:dark:text-dark-text-on-hover hover:text-light-text-on-hover dark:hover:bg-dark-surface-hover xss:text-[10px] lg:text-[12px]'>SignUp</Link>
+                    <Link href={"/Signup"} className='bg-light-surface px-4 py-1 cursor-pointer rounded-md hover:bg-light-surface-hover transition duration-300 dark:bg-dark-surface/20  dark:text-dark-text-primary text-light-text-primary hover:dark:text-dark-text-on-hover hover:text-light-text-on-hover dark:hover:bg-dark-surface-hover xss:text-[10px] lg:text-[12px]'>SignUp</Link>
                     <Link href={"/Billing"} className='xss:text-[10px] lg:text-[12px] hover:text-light-text-on-hover transition-all duration-300 dark:hover:text-dark-text-on-hover text-light-text-primary dark:text-dark-text-primary'>Pricing</Link>
                     <div><ThemeToggler /></div>
                 </div>
