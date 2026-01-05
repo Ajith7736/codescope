363ff44ec97f0af5f77f48479a041a65a5a69b02
Ajith7736 <ajith.aju39502@gmail.com>
2026-01-05 12:15:44 +0530

    ui fixes

--- src/features/home/components/seperator/SeperatorLine.tsx
+++ src/features/home/components/seperator/SeperatorLine.tsx
@@ -4,8 +4,8 @@ import { motion } from "framer-motion"
 function SeperatorLine() {
   return (
     <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} className='flex gap-5'>
-        <div className='h-px w-[180px] bg-linear-to-r from-dark-background via-dark-background to-indigo-500/80'></div>
-        <div className='h-px w-[180px] bg-linear-to-r to-dark-background via-dark-background from-indigo-500/80'></div>
+        <div className='h-px w-[180px] bg-linear-to-r dark:from-dark-background dark:via-dark-background to-indigo-500/80'></div>
+        <div className='h-px w-[180px] bg-linear-to-r dark:to-dark-background dark:via-dark-background from-indigo-500/80'></div>
     </motion.div>
   )
 }
--- src/features/home/layout/Content.tsx
+++ src/features/home/layout/Content.tsx
@@ -48,7 +48,7 @@ function Content({ session }: { session: Session | null }) {
             </header>
 
 
-            <section className='relative flex flex-col md:flex-wrap px-4 gap-5 md:flex-row z-10 md:items-center justify-center bg-light-border/20 dark:bg-dark-surface-hover/10 w-full'>
+            <section className='relative flex flex-col md:flex-wrap px-4 py-4 gap-5 md:flex-row z-10 md:items-center justify-center bg-light-border/20 dark:bg-dark-surface-hover/10 w-full'>
                 {cards.map((card) => {
                     return <Card key={card.title} logo={card.logo} title={card.title} desc={card.desc} />
                 })}
